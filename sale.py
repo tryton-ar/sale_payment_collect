583af590a0ee3dede584f13a47e225e76278c663
Luciano Rossi <lukio@gcoop.coop>
2024-08-29 17:55:07 -0300

    Update to works with invoice_party

--- sale.py
+++ sale.py
@@ -3,28 +3,43 @@
 
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
-from trytond.pyson import Eval
+from trytond.pyson import Eval, If, Bool
 
 
 class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
 
     paymode = fields.Many2One('payment.paymode', 'Paymode',
-        domain=[('party', '=', Eval('party', -1))], depends=['party'])
+        domain=[
+            ('party', '=', If(Bool(Eval('invoice_party',)),
+                    Eval('invoice_party'), Eval('party'))),
+            ],
+        states={
+            'readonly': Eval('state') != 'draft',
+            })
 
-    def __get_paymode(self):
-        '''
-        Return paymode.
-        '''
-        if self.party:
-            if self.party.customer_paymode:
-                self.paymode = self.party.customer_paymode
-
-    @fields.depends('party', 'paymode')
+    @fields.depends('party', 'invoice_party', 'paymode')
     def on_change_party(self):
         super(Sale, self).on_change_party()
+
+        if not self.invoice_party:
+            self.paymode = None
+        if self.party:
+            if not self.invoice_party:
+                if self.party.customer_paymode:
+                    self.paymode = self.party.customer_paymode
+
+    @fields.depends('party', 'invoice_party', 'paymode')
+    def on_change_invoice_party(self):
+        super(Sale, self).on_change_invoice_party()
+
         self.paymode = None
-        self.__get_paymode()
+        if self.invoice_party:
+            if self.invoice_party.customer_paymode:
+                self.paymode = self.invoice_party.customer_paymode
+        elif self.party:
+            if self.party.customer_paymode:
+                self.paymode = self.party.customer_paymode
 
     def _get_invoice_sale(self):
         invoice = super(Sale, self)._get_invoice_sale()
